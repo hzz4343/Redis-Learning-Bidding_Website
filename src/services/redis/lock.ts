48bedaa1fe3faa7681c7ff0f87d38f2380d43099
Zizhang He <hzz1996@qq.com>
2024-01-28 20:12:55 -0600

    Update Withlock

--- src/services/redis/lock.ts
+++ src/services/redis/lock.ts
@@ -1,4 +1,34 @@
-export const withLock = async () => {};
+import { client } from './client';
+import { randomBytes } from 'crypto';
+
+export const withLock = async (key: string, cb: () => any) => {
+	const retryDelayMs = 100;
+	let retries = 20;
+
+	const token = randomBytes(6).toString('hex');
+	const lockKey = `lock:${key}`;
+
+	while (retries >= 0) {
+		retries--;
+
+		const acquired = await client.set(lockKey, token, {
+			NX: true,
+			PX: 2000
+		});
+
+		if (!acquired) {
+			await pause(retryDelayMs);
+			continue;
+		}
+		try {
+			const result = await cb();
+
+			return result;
+		} finally {
+			await client.del(lockKey);
+		}
+	}
+};
 
 const buildClientProxy = () => {};
 
